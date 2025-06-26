a01a9c86811ded63404f313778c30eb6e59723c1
yone-coder <147109802+yone-coder@users.noreply.github.com>
2025-06-26 00:15:55 -0400

    Update server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get('/api/test-db', async (req, res) => {
 
     // Test basic connection
     const { data, error } = await supabase
-      .from('profiles')
+      .from('users')
       .select('*')
       .limit(1);
 
