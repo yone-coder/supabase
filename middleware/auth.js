a340ef06c15cacbd73ea83b46ec2ac2d1ce21330
yone-coder <147109802+yone-coder@users.noreply.github.com>
2025-06-27 16:29:00 -0400

    Create middleware/auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,28 @@
+
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../config/constants');
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ 
+      success: false, 
+      message: 'Access token required' 
+    });
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ 
+        success: false, 
+        message: 'Invalid or expired token' 
+      });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = { authenticateToken };
