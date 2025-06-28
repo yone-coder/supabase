9c63913eea41ea4fd028c1a52d9fcb3446e005af
yone-coder <147109802+yone-coder@users.noreply.github.com>
2025-06-28 11:57:51 -0400

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const { supabase } = require('./database');
+const supabase = require('./database');
 
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
